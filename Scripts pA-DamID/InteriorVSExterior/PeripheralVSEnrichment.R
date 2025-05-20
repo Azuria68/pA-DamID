a68095a226a4aefc87cac7cff1720ed41e46ed74
Tom Lanchec <tom.lanchec@outlook.fr>
2025-05-20 21:03:57 +0200

    Update PeripheralVSEnrichment.R

--- Scripts pA-DamID/InteriorVSExterior/PeripheralVSEnrichment.R
+++ Scripts pA-DamID/InteriorVSExterior/PeripheralVSEnrichment.R
@@ -1,12 +1,9 @@
-# Chargement des librairies
 library(tidyverse)
 library(ggbeeswarm)
 library(RColorBrewer)
 
-# Dossier des données
 analysis_dir <- '/Users/tom/Downloads/m6A/030325pADamRbLBR/analysis_results/'
 
-# Lecture des fichiers CSV
 periphery_m6A <- read.csv(file.path(analysis_dir, "periphery_m6A.csv"))
 interior_m6A  <- read.csv(file.path(analysis_dir, "interior_m6A.csv"))
 periphery_IF  <- read.csv(file.path(analysis_dir, "periphery_IF.csv"))
@@ -14,7 +11,6 @@ interior_IF   <- read.csv(file.path(analysis_dir, "interior_IF.csv"))
 periphery_neg <- read.csv(file.path(analysis_dir, "periphery_neg.csv"))
 interior_neg  <- read.csv(file.path(analysis_dir, "interior_neg.csv"))
 
-# Fonction pour appariement et calcul d'enrichissement
 match_and_compute_enrichment <- function(periphery, interior, label, max_distance = 20) {
   matched <- periphery %>%
     rowwise() %>%
@@ -25,15 +21,12 @@ match_and_compute_enrichment <- function(periphery, interior, label, max_distanc
   return(matched)
 }
 
-# Calcul enrichissement pour chaque condition
 m6A_enrichment <- match_and_compute_enrichment(periphery_m6A, interior_m6A, "LBR_m6A")
 IF_enrichment  <- match_and_compute_enrichment(periphery_IF, interior_IF, "LBR_IF")
 neg_enrichment <- match_and_compute_enrichment(periphery_neg, interior_neg, "neg (Dam)")
 
-# Combinaison de toutes les conditions
 combined <- bind_rows(m6A_enrichment, IF_enrichment, neg_enrichment)
 
-# Graphique enrichissements
 plt <- ggplot(combined, aes(x = marker, y = enrichment, color = marker)) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "grey50") +
   geom_quasirandom(size = 1.5) +
@@ -45,6 +38,5 @@ plt <- ggplot(combined, aes(x = marker, y = enrichment, color = marker)) +
 
 print(plt)
 
-# Sauvegarde résultats
 write.table(combined, file = "combined_enrichment_results.txt", 
             sep = "\t", quote = FALSE, row.names = FALSE)
